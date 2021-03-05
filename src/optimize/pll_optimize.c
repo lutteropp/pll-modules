ed57862591367fccf1c61f945ac91f81488bb25d
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-03-05 14:53:11 +0100

    adapted to changed submodule

--- src/optimize/pll_optimize.c
+++ src/optimize/pll_optimize.c
@@ -330,12 +330,20 @@ static double compute_negative_lnl_unrooted (void * p, double *x)
       scaler = partition->scale_buffer[params->lk_params.where.rooted_t.scaler_index];
     }
 
-    score = -1
-        * pll_compute_root_loglikelihood (
+    /*score = -1
+        * pll_compute_root_loglikelihood_new (
             partition,
             params->lk_params.where.rooted_t.root_clv_index,
             partition->clv[params->lk_params.where.rooted_t.root_clv_index],
             scaler,
+            params->lk_params.params_indices,
+            NULL);*/
+
+    score = -1
+        * pll_compute_root_loglikelihood_old (
+            partition,
+            params->lk_params.where.rooted_t.root_clv_index,
+            params->lk_params.where.rooted_t.scaler_index,
             params->lk_params.params_indices,
             NULL);
   }
